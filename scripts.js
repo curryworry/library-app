ac1e5a2b168a584d770511efe666c4ae138ba351
Ash Sundaram <ashwinrsundaram@gmail.com>
2021-11-09 20:17:02 +1300

    Defined function to get book name, author and read status from user

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function getUserInput(){
+    let bookName = prompt("Enter Book Name");
+    let authorName = prompt("Enter Author Name");
+    let readStatus = prompt("Have you read this book? (y/n)");
+}
+
+//function Book(name,author,quote,readStatus){
+
+//}

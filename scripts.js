e473dd0cb8d4c03558800f93cb88e4e4ed395ce6
Ash Sundaram <ashwinrsundaram@gmail.com>
2021-11-09 20:27:19 +1300

    Created Book object and a function add books to the library

--- scripts.js
+++ scripts.js
@@ -1,9 +1,22 @@
+let myLibrary = [];
 function getUserInput(){
     let bookName = prompt("Enter Book Name");
     let authorName = prompt("Enter Author Name");
     let readStatus = prompt("Have you read this book? (y/n)");
+    return [bookName,authorName,readStatus];
 }
 
-//function Book(name,author,quote,readStatus){
+function Book(name,author,readStatus){
+    this.name = name;
+    this.author = author;
+    this.readStatus = readStatus;
+}
 
-//}
+function addBookToLibrary(){
+    let userInput = getUserInput();
+    let newBook = new Book(userInput[0],userInput[1],userInput[2]);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+}
+
+addBook();
